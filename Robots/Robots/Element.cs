805676b323208b6ffd7de335fa89f26de2284cfe
Alexander Grünn <190016@studierende.htl-donaustadt.at>
2021-10-23 19:23:15 +0200

    added classes for player etc.

---
+++ Robots/Robots/Element.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robots
+{
+    public abstract class Element
+    {
+        public char Symbol { get; private set; }
+
+
+        public override string ToString()
+        {
+            return Symbol.ToString();
+        }
+    }
+}
