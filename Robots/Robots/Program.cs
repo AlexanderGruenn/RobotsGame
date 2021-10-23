402b383a6d63d887ec7432b2c915232a4e054bff
Alexander Grünn <190016@studierende.htl-donaustadt.at>
2021-10-23 16:24:49 +0200

    introduced max size playing field

--- Robots/Robots/Program.cs
+++ Robots/Robots/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Robots
 {
-    class Program
+    public class Program
     {
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(System.IntPtr hWnd, int cmdShow);
+
+        static void Maximize()
+        {
+            Process p = Process.GetCurrentProcess();
+            ShowWindow(p.MainWindowHandle, 3);
+        }
+
         static void Main(string[] args)
         {
+            Maximize();
+            int maxWidth = Console.LargestWindowWidth;
+            int maxHeight = Console.LargestWindowWidth;
+
+
+            Console.ReadKey();
         }
     }
 }
